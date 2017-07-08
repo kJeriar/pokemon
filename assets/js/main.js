00e5eac44685b4d95c8d7eb81541b68ddd980043
kJeriar <karla.jeria@gmail.com>
2017-07-08 17:22:55 -0400

    accsesorios pg, footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,6 @@ $(document).ready(function() {
         result.pokemon_entries.forEach(function(pokemon, i) {
 
 
-            var img=i+1;
-            $(".pokemon").append("<div class='elemento'><a href='#' id='modal" + numero + "'><img src='http://pokeapi.co/media/img/" + numero + ".png'>" + element.pokemon_species.name + "</a></div>")
-        $("#modal" + numero).click(function() {
-        console.log("existe" + numero);
-        $('#myModal').modal('show')
-
-
         });
 
 
