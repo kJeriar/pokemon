5fdbf7216494b13c3ff85efde3ccf44afc612229
kJeriar <karla.jeria@gmail.com>
2017-07-08 17:06:06 -0400

    icono

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-    // Llama end point de todos los pokemones
+    // Llama a la api con el nombre de todos los pokemon
     $.get('http://pokeapi.co/api/v2/pokedex/1/', function(result) {
         console.log(result);
-        result.pokemon_entries.forEach(function(pokemon) {
-            $('.pokemon').append('<div>' + pokemon.pokemon_species.name + '</div>')
+        result.pokemon_entries.forEach(function(pokemon, i) {
+
+
+            var img=i+1;
+            $(".pokemon").append("<div class='elemento'><a href='#' id='modal" + numero + "'><img src='http://pokeapi.co/media/img/" + numero + ".png'>" + element.pokemon_species.name + "</a></div>")
+        $("#modal" + numero).click(function() {
+        console.log("existe" + numero);
+        $('#myModal').modal('show')
+
+
         });
 
 
