158947b958a01bca7b6c8f5f6fa291f6ca328a63
kJeriar <karla.jeria@gmail.com>
2017-07-08 22:05:10 -0400

    intento de mostrar img

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-   // Llama end point de todos los pokemones
-   $.get('http://pokeapi.co/api/v2/pokedex/1/', function(result) {
-       console.log(result);
-       result.pokemon_entries.forEach(function(pokemon) {
+   // Llama a todos los pokemones
+   $.get('http://pokeapi.co/api/v2/pokedex/1/', function(resultado) {
+       console.log(resultado);
+       resultado.pokemon_entries.forEach(function(pokemon) {
            $('.pokemon').append('<div>' + pokemon.pokemon_species.name + '</div>')
        });
 
@@ -12,3 +12,9 @@ $(document).ready(function() {
 
 
 
+//x.pokemon_entries.forEach(function(pokemon, i){
+//var numero = i + 1;
+//$(".pokemon").append("<div class='elemento'><a href='#' id='modal" + numero + "'><img src='http://pokeapi.co/media/img/" + numero + ".png'>" + element.pokemon_species.name + "</a></div>")
+//$("#modal" + numero).click(function() {
+//$('#myModal').modal('show')
+//
